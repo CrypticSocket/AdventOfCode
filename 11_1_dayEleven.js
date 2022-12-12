0d253401062c337ff296d46169cd54b520e47ae0
Saket Vaibhav <saket.vaibhav1197@gmail.com>
2022-12-13 00:10:55 +0530

    Day 11, huge numbers, great trick

---
+++ 11_1_dayEleven.js
@@ -0,0 +1,116 @@
+let input1 = `Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1`
+
+function start()
+{
+    let data = input.split('\n')
+    let monkeyInfo = processData(data)
+    let numberOfRounds = 20
+    let inspections = []
+    for (let i = 0; i < numberOfRounds; i++) {
+        inspections = workWithDemMonkeys(monkeyInfo)
+    }
+    console.log(inspections.sort((a, b) => b - a).splice(0, 2).reduce((a, b) => a * b))
+}
+
+function processData(data) {
+
+    let monkeyInfo = {}
+    let currentMonkey = ''
+    
+    data.forEach(line => {
+        let lineData = line.trim().split(' ')
+        let whatType = lineData[0]
+        // console.log(whatType)
+        switch(whatType)
+        {
+            case 'Monkey' : 
+                currentMonkey = lineData[1].replace(':','')
+                break;
+            case 'Starting' :
+                let items = line.trim().replace('Starting items: ', '').split(',').map(Number)
+                monkeyInfo[currentMonkey] = {items : items.reverse(), inspections : 0}
+                break;
+            case 'Operation:' :
+                let [type, amount] = line.trim().replace('Operation: new = old ', '').split(' ')
+                monkeyInfo[currentMonkey].operation = {type : type, amount : amount}
+                break;
+            case 'Test:' :
+                let divisibility = Number(lineData[lineData.length - 1])
+                monkeyInfo[currentMonkey].divisibility = divisibility
+                break;
+            case 'If' :
+                let toMonkey = Number(lineData[lineData.length - 1])
+                monkeyInfo[currentMonkey].test = monkeyInfo[currentMonkey].test ?? {} 
+                lineData[1] == 'true:' ? monkeyInfo[currentMonkey].test.correct = toMonkey : monkeyInfo[currentMonkey].test.wrong = toMonkey
+        }
+    })
+
+    return monkeyInfo
+}
+
+function workWithDemMonkeys(monkeyInfo) 
+{
+    let inspections = []
+    for (let i = 0; monkeyInfo[i]; i++) 
+    {
+        while(monkeyInfo[i].items.length)
+        {
+            monkeyInfo[i].inspections++
+            let currentItem = monkeyInfo[i].items.pop()
+            let worry = CalculateWorry(currentItem, monkeyInfo[i].operation)
+            if(worry % monkeyInfo[i].divisibility == 0)
+            {
+                PrependToArray(monkeyInfo[monkeyInfo[i].test.correct], worry)
+            }
+            else
+            {
+                PrependToArray(monkeyInfo[monkeyInfo[i].test.wrong], worry)
+            }
+        }
+        inspections.push(monkeyInfo[i].inspections)
+    }
+    return inspections
+}
+
+function CalculateWorry(currentItem, operation)
+{
+    amount = operation.amount == "old" ? currentItem : Number(operation.amount)
+    switch(operation.type)
+    {
+        case '+' : return currentItem + amount
+        case '*' : return currentItem * amount
+    }
+}
+
+function PrependToArray(currentArray, newItem)
+{
+    currentArray.items = [newItem, ...currentArray.items]
+}
+
+start()
